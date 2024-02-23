041701f816fec227b4c5d3b37224caeb80c9c964
Anand <anand.bhaskaran@beekeeper.io>
2024-02-23 14:36:28 +0100

    first commit

---
+++ home/src/components/Content.vue
@@ -0,0 +1,12 @@
+<template>
+  <div style="color: red;">{{ title }}</div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      title: "Welcome to Remote Component in Action..",
+    };
+  },
+};
+</script>
